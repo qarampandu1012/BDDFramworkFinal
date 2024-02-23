bee58e7d7b66b8ef2847f9af8b1de02e0c292d2e
Ram <veeraramapandu1012@gmail.com>
2024-02-23 09:18:15 +0530

    Create TestngTest Runner class in bdd framwork

---
+++ src/test/java/run/config/TestngTestRunner.java
@@ -0,0 +1,21 @@
+package run.config;
+
+import io.cucumber.testng.*;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+
+
+@CucumberOptions(
+
+		features = "C:\\Users\\veera\\OneDrive\\Desktop\\8AM AUTOMATION\\8am workspace\\BDDFramwork8Am\\src\\test\\java\\Features", glue = "StepDefinations", dryRun = false,
+
+		plugin = { "pretty", "summary",
+
+				"html:TestReport/CucumberReportHtml/Report.html" },
+
+		//tags = "@sanity" 
+		tags="@Regresion"
+)
+
+public class TestngTestRunner extends AbstractTestNGCucumberTests {
+
+}
